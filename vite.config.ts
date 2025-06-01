d4ac155acb68f06f8455df7a8d993cddcad67af1
ColKernel0x8E <84617346+ColKernel0x8E@users.noreply.github.com>
2025-06-01 20:24:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    base: `/virtual-abacus/`,
+export default defineConfig(({ mode }) => {   
     const env = loadEnv(mode, '.', '');
     return {
+      base: `/virtual-abacus/`,
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
